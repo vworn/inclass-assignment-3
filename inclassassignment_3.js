5231efb53b7d23e3dba9c4b010af5ed8fb177045
vworn <jasonkmathewpersonal@gmail.com>
2025-02-13 14:51:41 -0500

    Task 6

--- inclassassignment_3.js
+++ inclassassignment_3.js
@@ -14,3 +14,27 @@ console.log(findOrders(orders, 101));
 let some_order = findOrders(orders, 101);
 console.log(`${some_order.customer} has an order id: ${some_order.id} with a total of: $${some_order.total}`);
 
+// Task 6
+
+let inventory = {
+    items: [],
+    addItem(name, quantity) {
+        this.items.push({ name, quantity });
+    },// This method removes lastest item
+    removeLatestItem() {
+        this.items.pop();
+    },//this method removes the first item
+    removeFirstItem() {
+        this.items.shift();
+    }
+};
+
+inventory.addItem("Monitor", 5);
+inventory.addItem("Keyboard", 10);
+inventory.addItem("Mouse", 3);
+
+console.log(inventory.items); // Viewing all items
+inventory.removeLatestItem(); // Removing latest
+console.log(inventory.items); // View items remaining
+inventory.removeFirstItem(); // Removing first 
+console.log(inventory.items); // View items remaining
